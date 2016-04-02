68ad0ff1e89f6ee5e605aafce3f60ce9fa01a90e
Kevin Kirsche <Kev.Kirsche@gmail.com>
2016-04-02 17:10:43 -0400

    Expand the struct to fully encompass all that could be within a config file
    Signed-off-by: Kevin Kirsche <Kev.Kirsche@gmail.com>

--- lib/entryGenerator.go
+++ lib/entryGenerator.go
@@ -1,48 +1,24 @@
 package sshConfig
 
-// SSHConfigurationEntry represents the data which can be used to generate an
-// SSH Configuration File Entry.
-type SSHConfigurationEntry struct {
-	AddressFamily                   string
-	BatchMode                       string
-	BindAddress                     string
-	ChallengeResponseAuthentication string
-	CheckHostIP                     string
-	Ciphers                         string
-	Compression                     string
-	CompressionLevel                int
-	ConnectionAttempts              int
-	ConnectTimeout                  int
-	ExitOnForwardFailure            string
-	ForwardAgent                    string
-	IdentitiesOnly                  string
-	IdentityFile                    string
-	Host                            string
-	Hostname                        string
-	Port                            int
-	User                            string
-}
-
 const (
 	// SSHConfigurationEntryTemplate holds the text template used to generate a
 	// new SSH configuration file entry.
 	SSHConfigurationEntryTemplate = `Host {{ .Host}}
 {{- if  .AddressFamily }}{{ print "\n" }}AddressFamily {{.AddressFamily}}{{- end}}
-{{- if  .BatchMode }}{{ print "\n" }}BatchMode {{.BatchMode}} # Valid options: yes or no. Default: no.{{- end }}
+{{- if  .BatchMode }}{{ print "\n" }}BatchMode {{.BatchMode}}{{- end }}
 {{- if  .BindAddress }}{{ print "\n" }}BindAddress {{.BindAddress}}{{- end}}
-{{- if  .ChallengeResponseAuthentication }}{{ print "\n" }}ChallengeResponseAuthentication {{.ChallengeResponseAuthentication}} # Valid options: yes or no. Default: yes{{- end}}
-{{- if  .CheckHostIP }}{{ print "\n" }}CheckHostIP {{.CheckHostIP}} # Valid options: yes or no. Default: yes{{- end}}
+{{- if  .ChallengeResponseAuthentication }}{{ print "\n" }}ChallengeResponseAuthentication {{.ChallengeResponseAuthentication}}{{- end}}
+{{- if  .CheckHostIP }}{{ print "\n" }}CheckHostIP {{.CheckHostIP}}{{- end}}
 {{- if  .Ciphers }}{{ print "\n" }}Ciphers {{.Ciphers}}{{- end}}
-{{- if  .Compression }}{{ print "\n" }}Compression {{.Compression}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .Compression }}{{ print "\n" }}Compression {{.Compression}}{{- end}}
 {{- if  .ConnectionAttempts }}{{ print "\n" }}ConnectionAttempts {{.ConnectionAttempts}}{{- end}}
 {{- if  .ConnectTimeout }}{{ print "\n" }}ConnectTimeout {{.ConnectTimeout}} # In seconds{{- end}}
-{{- if  .DynamicForward }}{{ print "\n" }}DynamicForward {{.DynamicForward}}{{- end}}
-{{- if  .ExitOnForwardFailure }}{{ print "\n" }}ExitOnForwardFailure {{.ExitOnForwardFailure}} # Valid options: yes or no. Default: no{{- end}}
-{{- if  .ForwardAgent }}{{ print "\n" }}ForwardAgent {{.ForwardAgent}} # Valid options: yes or no. Default: no{{- end}}
-{{- if  .IdentitiesOnly }}{{ print "\n" }}IdentitiesOnly {{.IdentitiesOnly}} # Default: no{{- end}}
+{{- if  .ExitOnForwardFailure }}{{ print "\n" }}ExitOnForwardFailure {{.ExitOnForwardFailure}}{{- end}}
+{{- if  .ForwardAgent }}{{ print "\n" }}ForwardAgent {{.ForwardAgent}}{{- end}}
+{{- if  .IdentitiesOnly }}{{ print "\n" }}IdentitiesOnly {{.IdentitiesOnly}}{{- end}}
 {{- if  .IdentityFile }}{{ print "\n" }}IdentityFile {{.IdentityFile}}{{- end}}
 {{- if  .Hostname }}{{ print "\n" }}Hostname {{.Hostname}}{{- end}}
 {{- if  .Port }}{{ print "\n" }}Port {{.Port}}{{- end}}
-{{- if  .User }}{{ print "\n" }}User {{.}}{{- end}}
+{{- if  .User }}{{ print "\n" }}User {{.User}}{{- end}}
 `
 )
