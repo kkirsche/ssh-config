aefec65c5d3230fc6a3a67d126bd2a6a0a827c09
Kevin Kirsche <Kev.Kirsche@gmail.com>
2016-04-02 10:48:44 -0400

    Begin expanding the add command
    Signed-off-by: Kevin Kirsche <Kev.Kirsche@gmail.com>

--- cmd/add.go
+++ cmd/add.go
@@ -17,35 +17,33 @@ package cmd
 import (
 	"html/template"
 	"os"
-	"os/user"
 
+	"github.com/kkirsche/ssh-config/lib"
 	"github.com/spf13/cobra"
 )
 
-var hostNickname string
-var remoteHost string
-var remotePort int
-var remoteUsername string
-
-type configEntry struct {
-	Host     string
-	Hostname string
-	Port     int
-	User     string
-}
-
-const (
-	sshConfigTemplate = `Host {{ .Host}}
-{{with .Hostname -}}
-Hostname {{.}}
-{{- end}}
-{{with .Port -}}
-Port {{.}}
-{{- end}}
-{{with .User -}}
-User {{.}}
-{{- end}}`
-)
+var addressFamily string
+var batchmode string
+var bindAddress string
+var challengeRespAuth string
+var checkHostIP string
+var ciphers string
+var clearAllForwardings string
+var compression string
+var compressionLevel int
+var connectionAttempts int
+var connectTimeout int
+var exitOnForwardFailure string
+var forwardAgent string
+var forwardX11 string
+var forwardX11Trusted string
+var hashKnownHosts string
+var identitiesOnly string
+var identityFile string
+var host string
+var hostname string
+var port int
+var username string
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
@@ -58,48 +56,61 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		config := configEntry{
-			Host:     "Ghost",
-			Hostname: "1.2.3.4",
-			Port:     22,
-			User:     "WTF BBQ",
+		if host == "" {
+			logger.Errorf("Name not provided. Please provide the name via --name or -n for host %s", hostname)
+		}
+		if hostname == "" {
+			logger.Errorf("Remote host not provided. Please provide the name via --remote-host or -r for named entry: %s", host)
 		}
 
-		t := template.Must(template.New("sshConfig").Parse(sshConfigTemplate))
-		t.Execute(os.Stdout, config)
+		if host != "" && hostname != "" {
+			config := sshConfig.SSHConfigurationEntry{
+				AddressFamily:                   addressFamily,
+				BatchMode:                       batchmode,
+				BindAddress:                     bindAddress,
+				ChallengeResponseAuthentication: challengeRespAuth,
+				CheckHostIP:                     checkHostIP,
+				Ciphers:                         ciphers,
+				Compression:                     compression,
+				ConnectionAttempts:              connectionAttempts,
+				ConnectTimeout:                  connectTimeout,
+				ExitOnForwardFailure:            exitOnForwardFailure,
+				ForwardAgent:                    forwardAgent,
+				ForwardX11:                      forwardX11,
+				ForwardX11Trusted:               forwardX11Trusted,
+				HashKnownHosts:                  hashKnownHosts,
+				IdentitiesOnly:                  identitiesOnly,
+				IdentityFile:                    identityFile,
+				Host:                            host,
+				Hostname:                        hostname,
+				Port:                            port,
+				User:                            username,
+			}
+
+			t := template.Must(template.New("sshConfig").Parse(sshConfig.SSHConfigurationEntryTemplate))
+			err := t.Execute(os.Stdout, config)
+			if err != nil {
+				logger.Errorf("Couldn't execute SSH Configuration Template with error: %s", err.Error())
+			}
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-
-	// Nicknames
-	addCmd.PersistentFlags().StringVarP(&hostNickname, "name", "n", "",
-		`The name is the name argument given on the command
-			     line to sshd when connecting to the remote host.`)
-
-	// #  Remote Machine Details
-	// ## Connection Details
-	addCmd.PersistentFlags().StringVarP(&remoteHost, "remote-host", "r", "",
-		`The remote IP or hostname to connect to using SSH.`)
-	addCmd.PersistentFlags().IntVarP(&remotePort, "port", "p", 22, `The remote sshd port.`)
-
-	var err error
-	currentUser, err = user.Current()
-	if err != nil {
-		logger.Errorf("Could not load current user with error: %s", err.Error())
-	}
-	if currentUser != nil {
-		addCmd.PersistentFlags().StringVarP(&remoteUsername, "username", "u", currentUser.Name, `The remote SSH username.`)
-	}
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	addCmd.PersistentFlags().StringVarP(&addressFamily, "address-family", "a", "", "Specifies which address family to use\n\t\t\t\t\t when connecting. Valid arguments are\n\t\t\t\t\t 'any', 'inet' (use IPv4 only), or\n\t\t\t\t\t 'inet6' (use IPv6 only).")
+	addCmd.PersistentFlags().StringVarP(&batchmode, "batchmode", "b", "", "If set to 'yes', passphrase/password\n\t\t\t\t\t querying will be disabled. This option\n\t\t\t\t\t is useful in scripts and other batch\n\t\t\t\t\t jobs where no user is present to supply\n\t\t\t\t\t the password. The argument must be\n\t\t\t\t\t 'yes' or 'no'. The default is 'no'.")
+	addCmd.PersistentFlags().StringVarP(&bindAddress, "bind-address", "d", "", "Use the specified address on the local\n\t\t\t\t\t machine as the source address of the\n\t\t\t\t\t connection. Only useful on systems with\n\t\t\t\t\t more than one address. Note that this\n\t\t\t\t\t option does not work if\n\t\t\t\t\t UsePrivilegedPort is set to 'yes'.")
+	addCmd.PersistentFlags().StringVarP(&checkHostIP, "check-host-ip", "e", "", "If this flag is set to 'yes', ssh will\n\t\t\t\t\t additionally check the host IP address\n\t\t\t\t\t in the known_hosts file. This allows\n\t\t\t\t\t ssh to detect if a host key changed due\n\t\t\t\t\t to DNS spoofing. If the option\n\t\t\t\t\t is set to 'no', the check will not be\n\t\t\t\t\t executed. The default is 'yes'.")
+	addCmd.PersistentFlags().StringVarP(&forwardAgent, "forward-agent", "f", "", "Specifies whether the connection to the\n\t\t\t\t\t authentication agent (if any) will be\n\t\t\t\t\t forwarded to the remote machine.\n\t\t\t\t\t The argument must be 'yes' or 'no'.\n\t\t\t\t\t The default is 'no'.\n\n\t\t\t\t\t Agent forwarding should be enabled with\n\t\t\t\t\t caution. Users with the ability to\n\t\t\t\t\t bypass file permissions on the\n\t\t\t\t\t remote host (for the agent's\n\t\t\t\t\t Unix-domain socket) can access the\n\t\t\t\t\t local agent through the forwarded\n\t\t\t\t\t connection. An attacker cannot obtain\n\t\t\t\t\t key material from the agent, however\n\t\t\t\t\t they can perform operations on the keys\n\t\t\t\t\t that enable them to authenticate using\n\t\t\t\t\t the identities loaded into the agent.")
+	addCmd.PersistentFlags().StringVarP(&exitOnForwardFailure, "exit-on-forward-failure", "i", "", "Specifies whether ssh should\n\t\t\t\t\t terminate the connection if it cannot\n\t\t\t\t\t set up all requested dynamic, tunnel,\n\t\t\t\t\t local, and remote port forwardings.\n\t\t\t\t\t The argument must be 'yes' or 'no'.\n\t\t\t\t\t The default is 'no'.")
+	addCmd.PersistentFlags().StringVarP(&challengeRespAuth, "challenge-response-auth", "l", "", "Specifies whether to use\n\t\t\t\t\t challenge-response authentication.\n\t\t\t\t\t The argument to this keyword must be\n\t\t\t\t\t 'yes' or 'no'. The default is 'yes'.")
+	addCmd.PersistentFlags().StringVarP(&compression, "compression", "m", "", "Specifies whether to use compression.\n\t\t\t\t\t The argument must be 'yes' or 'no'.\n\t\t\t\t\t The default is 'no'.")
+	addCmd.PersistentFlags().StringVarP(&host, "name", "n", "", "The name is the name argument given on\n\t\t\t\t\t the command line to sshd when\n\t\t\t\t\t connecting to the remote host.")
+	addCmd.PersistentFlags().IntVarP(&connectionAttempts, "connection-attempts", "o", 1, "Specifies the number of tries (one per\n\t\t\t\t\t second) to make before exiting.\n\t\t\t\t\t This may be useful in scripts if the\n\t\t\t\t\t connection sometimes fails.")
+	addCmd.PersistentFlags().IntVarP(&port, "port", "p", 22, "Specifies the port number to connect\n\t\t\t\t\t on the remote host.")
+	addCmd.PersistentFlags().StringVarP(&hostname, "hostname", "r", "", "Specifies the real host name to log\n\t\t\t\t\t into. This can be used to specify\n\t\t\t\t\t nicknames or abbreviations for hosts.\n\t\t\t\t\t The default is the name given\n\t\t\t\t\t on the command line. Numeric IP\n\t\t\t\t\t addresses are also permitted.")
+	addCmd.PersistentFlags().StringVarP(&ciphers, "ciphers", "s", "", "Specifies the ciphers allowed for\n\t\t\t\t\t protocol version 2 in order of\n\t\t\t\t\t preference. Multiple ciphers must be\n\t\t\t\t\t comma-separated. The supported ciphers\n\t\t\t\t\t are '3des-cbc', 'aes128-cbc',\n\t\t\t\t\t 'aes192-cbc', 'aes256-cbc',\n\t\t\t\t\t 'aes128-ctr', 'aes192-ctr',\n\t\t\t\t\t 'aes256-ctr', 'arcfour128',\n\t\t\t\t\t 'arcfour256', 'arcfour',\n\t\t\t\t\t 'blowfish-cbc', and 'cast128-cbc'.")
+	addCmd.PersistentFlags().IntVarP(&connectTimeout, "connect-timeout", "t", 0, "Specifies the timeout (in seconds) used\n\t\t\t\t\t when connecting to the SSH server,\n\t\t\t\t\t instead of using the default system\n\t\t\t\t\t TCP timeout. This value is used only\n\t\t\t\t\t when the target is down or really\n\t\t\t\t\t unreachable, not when it refuses\n\t\t\t\t\t the connection.")
+	addCmd.PersistentFlags().StringVarP(&username, "user", "u", "", "The remote username to connect as.")
 }
---
+++ lib/entryGenerator.go
@@ -0,0 +1,54 @@
+package sshConfig
+
+// SSHConfigurationEntry represents the data which can be used to generate an
+// SSH Configuration File Entry.
+type SSHConfigurationEntry struct {
+	AddressFamily                   string
+	BatchMode                       string
+	BindAddress                     string
+	ChallengeResponseAuthentication string
+	CheckHostIP                     string
+	Ciphers                         string
+	Compression                     string
+	CompressionLevel                int
+	ConnectionAttempts              int
+	ConnectTimeout                  int
+	ExitOnForwardFailure            string
+	ForwardAgent                    string
+	ForwardX11                      string
+	ForwardX11Trusted               string
+	HashKnownHosts                  string
+	IdentitiesOnly                  string
+	IdentityFile                    string
+	Host                            string
+	Hostname                        string
+	Port                            int
+	User                            string
+}
+
+const (
+	// SSHConfigurationEntryTemplate holds the text template used to generate a
+	// new SSH configuration file entry.
+	SSHConfigurationEntryTemplate = `Host {{ .Host}}
+{{- if  .AddressFamily }}{{ print "\n" }}AddressFamily {{.AddressFamily}}{{- end}}
+{{- if  .BatchMode }}{{ print "\n" }}BatchMode {{.BatchMode}} # Valid options: yes or no. Default: no.{{- end }}
+{{- if  .BindAddress }}{{ print "\n" }}BindAddress {{.BindAddress}}{{- end}}
+{{- if  .ChallengeResponseAuthentication }}{{ print "\n" }}ChallengeResponseAuthentication {{.ChallengeResponseAuthentication}} # Valid options: yes or no. Default: yes{{- end}}
+{{- if  .CheckHostIP }}{{ print "\n" }}CheckHostIP {{.CheckHostIP}} # Valid options: yes or no. Default: yes{{- end}}
+{{- if  .Ciphers }}{{ print "\n" }}Ciphers {{.Ciphers}}{{- end}}
+{{- if  .Compression }}{{ print "\n" }}Compression {{.Compression}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .ConnectionAttempts }}{{ print "\n" }}ConnectionAttempts {{.ConnectionAttempts}}{{- end}}
+{{- if  .ConnectTimeout }}{{ print "\n" }}ConnectTimeout {{.ConnectTimeout}} # In seconds{{- end}}
+{{- if  .DynamicForward }}{{ print "\n" }}DynamicForward {{.DynamicForward}}{{- end}}
+{{- if  .ExitOnForwardFailure }}{{ print "\n" }}ExitOnForwardFailure {{.ExitOnForwardFailure}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .ForwardAgent }}{{ print "\n" }}ForwardAgent {{.ForwardAgent}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .ForwardX11 }}{{ print "\n" }}ForwardX11 {{.ForwardX11}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .ForwardX11Trusted }}{{ print "\n" }}ForwardX11Trusted {{.ForwardX11Trusted}} # Valid options: yes or no. Default: no{{- end}}
+{{- if  .HashKnownHosts }}{{ print "\n" }}HashKnownHosts {{.HashKnownHosts}} # Default: no{{- end}}
+{{- if  .IdentitiesOnly }}{{ print "\n" }}IdentitiesOnly {{.IdentitiesOnly}} # Default: no{{- end}}
+{{- if  .IdentityFile }}{{ print "\n" }}IdentityFile {{.IdentityFile}}{{- end}}
+{{- if  .Hostname }}{{ print "\n" }}Hostname {{.Hostname}}{{- end}}
+{{- if  .Port }}{{ print "\n" }}Port {{.Port}}{{- end}}
+{{- if  .User }}{{ print "\n" }}User {{.}}{{- end}}
+`
+)
