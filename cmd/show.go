6270e31ce5e9955e649340f0755e9261902e649c
Kevin Kirsche <Kev.Kirsche@gmail.com>
2016-04-01 23:18:37 -0400

    Flesh out the help documentation for show
    Signed-off-by: Kevin Kirsche <Kev.Kirsche@gmail.com>

--- cmd/show.go
+++ cmd/show.go
@@ -24,13 +24,9 @@ import (
 // showCmd represents the show command
 var showCmd = &cobra.Command{
 	Use:   "show",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints the contents of the SSH configuration file.",
+	Long: `Prints the contents of the user's SSH configuration file or system SSH
+configuration file to Stdout.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logger.VerbosePrintf("Opening configuration file handle at %s", cfgFilePath)
 		file, err := os.Open(cfgFilePath)
